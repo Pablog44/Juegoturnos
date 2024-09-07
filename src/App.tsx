6b1b6a832091c5a853a811d647558c2f51ab9bcc
PABLOGONZALEZCUADRADO <pablogonzalezcuadrado@hotmail.com>
2024-09-07 05:27:26 +0200

    juego turnos avance
    -juego turnos avance

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import { ReactComponent as Logo } from './logo.svg'; // Importar el SVG como un componente
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Logo className="App-logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
